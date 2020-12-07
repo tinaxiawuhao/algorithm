38022d09d146a7d9c0311f0fd5cdd5cf536348dd
wuhao <wuhao.cosmo@haier.com>
2020-12-07 10:49:15 +0800

    非递归的快速排序

--- rankingAlgorithm/src/QuickSort.java
+++ rankingAlgorithm/src/QuickSort.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * @author wuhao
@@ -18,7 +19,11 @@ import java.util.Arrays;
 public class QuickSort {
     public static void main(String[] args) {
         int[] arr = {12, 32, 22, 7, 48, 3, 35, 6, 8, 42};
-        quickSort(arr, 0, arr.length - 1);
+//        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+
+        /*-----------非递归实现----------*/
+        sort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -55,6 +60,59 @@ public class QuickSort {
         // i的索引处为上面已确定好的基准值的位置，无需再处理
         quickSort(arr, left, i - 1);
         quickSort(arr, i + 1, right);
+
     }
 
+
+    /*-----------------------------非递归实现------------------------------*/
+    public static void sort(int[] arr, int left, int right) {
+        int privot, top, last;
+        Stack<Integer> s = new Stack<>();
+        privot = QuickSort(arr, left, right);
+        if (privot > left + 1) {
+            s.push(left);
+            s.push(privot - 1);
+
+        }
+        if (privot < right - 1) {
+            s.push(privot + 1);
+            s.push(right);
+        }
+        while (!s.empty()) {
+            top = s.pop();
+            last = s.pop();
+            privot = QuickSort(arr, last, top);
+            if (privot > last + 1) {
+                s.push(last);
+                s.push(privot - 1);
+            }
+            if (privot < top - 1) {
+                //System.out.println(top);
+                s.push(privot + 1);
+                s.push(top);
+            }
+        }
+    }
+
+    public static int QuickSort(int[] arr, int left, int right) {
+        int privot = left;
+        while (left < right) {
+            while ((arr[privot] < arr[right]) & left < right) {
+                right--;
+            }
+            int temp = arr[privot];
+            arr[privot] = arr[right];
+            arr[right] = temp;
+            privot = right;
+
+            while ((arr[privot] > arr[left]) & left < right) {
+                left++;
+            }
+            temp = arr[privot];
+            arr[privot] = arr[left];
+            arr[left] = temp;
+            privot = left;
+        }
+        return privot;
+    }
 }
